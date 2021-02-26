ba31fab549b898d20959245e3d0ffdc28558d8c7
Rimantas <belauzas@yahoo.com>
2021-02-26 14:07:43 +0200

    React update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import Logo from "./components/Logo.js";
+import Link from "./components/Link.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link />
       </header>
     </div>
   );
---
+++ src/components/Link.js
@@ -0,0 +1,13 @@
+function Link() {
+    return (
+        <a className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Musu awesome link
+        </a>
+    );
+}
+
+export default Link;
